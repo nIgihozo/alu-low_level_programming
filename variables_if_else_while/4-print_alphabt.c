ddb4faac4b56ea751cf48692dc6244a2ff38ceb6
nIgihozo <n.igihozo@alustudent.com>
2026-09-13 05:33:03 -0700

    Printing alphabets and skip q & e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet except e and q, followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
