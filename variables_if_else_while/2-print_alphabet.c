49bb96d12e4d4334d84576ab29b4ddb5ea2acd6c
nIgihozo <n.igihozo@alustudent.com>
2026-09-13 05:17:01 -0700

    Printing alphabet in small letters

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a random number and checks its value
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+            putchar(ch);
+    putchar('\n');
+	return (0);
+}
